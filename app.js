861e99c03a7ac6827f27de160e3828bd143b4455
GeoffMahugu <geoffreymahugu@gmail.com>
2021-03-18 12:00:54 +0300

    Added resolver and schema express binding

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const cors = require('cors');
 
 const { graphqlHTTP } = require('express-graphql');
 
-// const graphqlSchema = require('./graphql/schema');
-// const graphqlResolver = require('./graphql/resolvers');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 // # EXPRESS::INITIALIZE APP -/
 
@@ -22,14 +22,14 @@ app.use(cors());
 
 // # GRAPHQL::API SERVICE -/
 
-// app.use(
-//   '/graphql',
-//   graphqlHTTP({
-//     schema: graphqlSchema,
-//     rootValue: graphqlResolver,
-//     graphiql: true,
-//   })
-// );
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
 
 
 // # MONGOOSE::DATABASE SETUP -/
