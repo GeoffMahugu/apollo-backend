79c6aa15578e4b3232cbe11c82dce912c6a5959a
GeoffMahugu <geoffreymahugu@gmail.com>
2021-03-19 15:36:39 +0300

    Updated docs

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,6 +2,11 @@ const Product = require('../models/product');
 
 
 module.exports = {
+    /**
+     * 
+     * @returns Product 
+     * { name, description, price, discount, created_at, updated_at}
+     */
     products: async function() {
         const products = await Product.find();
         return {
@@ -13,6 +18,26 @@ module.exports = {
             })
         }
     },
+
+    /**
+     * 
+     * @param { name, description, price, discount} ProductInput
+     * @returns Product
+     * 
+     * @mutation 
+     * mutation {
+        createProduct(productInput: { name: "Test Product 1",description: "Test Product",price: 7000.50,discount: 10}){
+            _id,
+            name,
+            description,
+            price,
+            discount,
+            created_at,
+            updated_at
+        }
+       }
+     */
+
     createProduct: async function ({ productInput }) {
         // return productInput;
         const product = new Product({
