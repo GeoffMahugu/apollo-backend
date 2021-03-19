37a91b4d65c754d3f931e183ead651edd5e36742
GeoffMahugu <geoffreymahugu@gmail.com>
2021-03-19 14:57:22 +0300

    Update Product data model

--- graphql/schema.js
+++ graphql/schema.js
@@ -3,14 +3,18 @@ const { buildSchema } = require('graphql');
 module.exports = buildSchema(`
     type Product{
         _id:ID!
+        name: String!
         description: String!
         price: Float!
         discount: Int
+        created_at: Date
+        updated_at: Date
     }    
     type ProductData {
         products: [Product!]!
     }
     input ProductInputData {
+        name: String!
         description: String!
         price: Float!
         discount: Int
