a3b6ad8518667e9344c24cd34314794ebec51120
GeoffMahugu <geoffreymahugu@gmail.com>
2021-04-08 14:26:39 +0300

    Curr status

--- mongo.docker/mongo_init.js
+++ mongo.docker/mongo_init.js
@@ -1,11 +1,35 @@
+require('dotenv').config();
+
+
+// db.createUser(
+//     {
+//         user: "${process.env.MONGO_ROOT_USERNAME}",
+//         pwd: "${process.env.MONGO_ROOT_PASSWORD}",
+//         roles: [
+//             {
+//                 role: "readWrite",
+//                 db: "${process.env.MONGO_DATABASE_NAME}"
+//             }
+//         ]
+//     }
+// );
+db.auth('backend_admin', 'password');
+console.log('AUTHENTICATED -------------------------------------------');
+console.log(db);
+db = db.getSiblingDB('ecommerce');
+db.createCollection('TestDBConnectionDocuments'),
+    db.getCollection('TestDBConnectionDocuments'),
+
+    console.log('GET SIBLING -------------------------------------------');
+console.log(db);
 db.createUser(
     {
-        user: $MONGO_ROOT_USERNAME,
-        pwd: $MONGO_ROOT_PASSWORD,
+        user: "backend_admin",
+        pwd: "password",
         roles: [
             {
                 role: "readWrite",
-                db: $MONGO_DATABASE_NAME
+                db: "ecommerce"
             }
         ]
     }
