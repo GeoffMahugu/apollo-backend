c1ed5256f78e59f20e1dfd97639492261f058415
GeoffMahugu <geoffreymahugu@gmail.com>
2021-04-10 17:16:46 +0300

    Mongo init script

--- mongo.docker/mongo_init.js
+++ mongo.docker/mongo_init.js
@@ -1,34 +1,10 @@
-require('dotenv').config();
-
-
-// db.createUser(
-//     {
-//         user: "${process.env.MONGO_ROOT_USERNAME}",
-//         pwd: "${process.env.MONGO_ROOT_PASSWORD}",
-//         roles: [
-//             {
-//                 role: "readWrite",
-//                 db: "${process.env.MONGO_DATABASE_NAME}"
-//             }
-//         ]
-//     }
-// );
-// db.auth('backend_admin', 'password');
-// console.log('AUTHENTICATED -------------------------------------------');
-// console.log(db);
-// db = db.getSiblingDB('ecommerce');
-// db.createCollection('TestDBConnectionDocuments'),
-//     db.getCollection('TestDBConnectionDocuments'),
-
-//     console.log('GET SIBLING -------------------------------------------');
-// console.log(db);
 db.createUser(
     {
         user: "backend_admin",
         pwd: "password",
         roles: [
             {
-                role: "readWrite",
+                role: "root",
                 db: "ecommerce"
             }
         ]
